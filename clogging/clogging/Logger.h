17efd0c22228750d59af9f03321348f93e797746
David Michalik <dmicha16@student.aau.dk>
2018-02-12 23:15:49 +0100

    Working macro overloading

--- clogging/clogging/Logger.h
+++ clogging/clogging/Logger.h
@@ -23,15 +23,18 @@
 #define LOG 1
 #define JSON 2
 
-#define INIT_CLOGGING clogging::Logger ToLog;
+#define INIT_CLOGGING clogging::Logger ToLog
 
 //#define ADD_FILE(file_name, path)  ToLog.AddFile(file_name, path);
-#define ADD_FILE(file_name)		   ToLog.AddFile(file_name);
+#define ADD_FILE(file_name)		   ToLog.AddFile(file_name)
 
-#define CLOG(message, level, type) ToLog.Clog(message, level, type);
-#define CLOG(message, level)       ToLog.Clog(message, level);
-#define CLOG(message)			   ToLog.Clog(message);
-#define CLOG ToLog.Clog();
+#define GET_MACRO(_1,_2,_3,NAME,...) NAME
+#define CLOG(...) GET_MACRO(__VA_ARGS__, CLOG3, CLOG2, CLOG1, CLOG)(__VA_ARGS__)
+
+#define CLOG3(output_msg, level, type) ToLog.Clog(output_msg, level, type)
+#define CLOG2(output_msg, level) ToLog.Clog(output_msg, level)
+#define CLOG1(output_msg) ToLog.Clog(output_msg)
+#define CLOG() ToLog.Clog()
 
 using namespace std;
 
@@ -45,7 +48,7 @@ namespace clogging {
 	public:
 
 		void AddFile(string file_name);
-		void AddFile(string file_name, string path);
+		//void AddFile(string file_name, string path);
 
 		void Clog(string output_msg, string level, int specify_type);
 		void Clog(string output_msg, string level);
